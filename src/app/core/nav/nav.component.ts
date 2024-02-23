39b02f2d6d45a54698e74f42cb6afa9c293a7e1f
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-02-22 22:25:41 -0600

    Login button

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
