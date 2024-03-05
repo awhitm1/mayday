f88ef81e89f21490a86281fc2970320cf01e8bea
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-04 22:19:03 -0600

    implemented signup and autologin after signup

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,11 +1,13 @@
 export class User {
-  id!: number;
-  username: string = '';
-  email: string = '';
-  f_name: string = '';
-  l_name: string = '';
-  active: boolean = true;
-  is_admin: boolean = false;
-  is_tech: boolean = false;
+  email!: string;
+  f_name!: string;
+  l_name!: string;
+  id?: number;
+  active?: boolean = true;
+  is_admin?: boolean = false;
+  is_tech?: boolean = false;
   token?: string;
+  password?: string = '';
+  password_confirmation?: string = '';
+
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+
+export interface newUser {
+  f_name: string;
+  l_name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +26,11 @@ export class UserService {
 
   isTech(){
     return this.http.get<boolean>(`${environment.apiUrl}/users_is_tech`);
+  }
+
+  createUser(user: User | any){
+    console.log("f_name: ", user.f_name, "l_name: ", user.l_name, "email: ", user.email, "password: ", user.password, "password_confirmation: ", user.password_confirmation)
+    const newUser: newUser = {f_name: user.f_name, l_name: user.l_name, email: user.email, password: user.password, password_confirmation: user.password_confirmation};
+    return this.http.post<User>(`${environment.apiUrl}/users`, user);
   }
 }
