e4d5a3dd98b0c4b4f10c0e0569431d0af076af5b
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-15 21:54:48 -0500

    .

--- src/app/features/config/config-dialog/config-dialog.component.ts
+++ src/app/features/config/config-dialog/config-dialog.component.ts
@@ -18,11 +18,12 @@ import { Observable, map, startWith } from 'rxjs';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Group } from 'src/app/shared/models/group.model';
 import { User } from 'src/app/shared/models/user.model';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-config-dialog',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, MatFormField, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatSelectModule, MatIconModule, MatChipsModule, MatAutocompleteModule, AsyncPipe, FormsModule],
+  imports: [MatDialogTitle, MatDialogContent, MatFormField, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatSelectModule, MatIconModule, MatChipsModule, MatAutocompleteModule, AsyncPipe, FormsModule, MatSlideToggleModule],
   templateUrl: './config-dialog.component.html',
   styleUrl: './config-dialog.component.css'
 })
