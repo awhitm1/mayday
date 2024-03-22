0224a878e572233ead061ddf80961a0a64817370
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-21 19:10:47 -0500

    fix dialog buttons - make it close

--- src/app/features/ticket/ticket.component.ts
+++ src/app/features/ticket/ticket.component.ts
@@ -9,7 +9,7 @@ import { TicketService } from 'src/app/shared/services/ticket.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/shared/models/user.model';
 import { DialogData } from '../config/config.component';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Category } from 'src/app/shared/models/category.model';
 import { Status } from 'src/app/shared/models/status.model';
 import { Group } from 'src/app/shared/models/group.model';
@@ -24,6 +24,7 @@ import { Location } from 'src/app/shared/models/location.model';
 })
 
 export class TicketComponent implements OnInit{
+  dialogRef: MatDialogRef<TicketComponent>;
   viewOnly: boolean = false;
   selectedTicket: Ticket = this.data.ticket || new Ticket();
   isNewTicket = this.data.isNew;
@@ -42,7 +43,9 @@ export class TicketComponent implements OnInit{
     group_id: new FormControl(''),
   });
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private ticketService: TicketService, private router: Router, private formBuilder: FormBuilder) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private ticketService: TicketService, private router: Router, private formBuilder: FormBuilder, dialogRef: MatDialogRef<TicketComponent>) {
+    this.dialogRef = dialogRef;
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -66,7 +69,7 @@ export class TicketComponent implements OnInit{
     this.ticketService.createTicket(this.ticketForm.value).subscribe({
       next: (ticket: Ticket) => {
         console.log('Ticket created: ', ticket);
-        this.router.navigate(['/queue']);
+        this.dialogRef.close();
     },
     error: (error: any) => {
       console.error('Error creating ticket: ', error);
@@ -75,18 +78,31 @@ export class TicketComponent implements OnInit{
  }
 
   updateTicket(){
-
+    this.ticketService.updateTicket(this.ticketForm.value).subscribe({
+      next: (ticket: Ticket) => {
+        console.log('Ticket updated: ', ticket);
+        this.dialogRef.close();
+      },
+      error: (error: any) => {
+        console.error('Error updating ticket: ', error);
+      }
+    });
  }
 
  claimTicket(id: number){
     this.ticketService.claimTicket(id).subscribe({
       next: (ticket: Ticket) => {
         console.log('Ticket claimed: ', ticket);
-       this.router.navigate(['/queue']);
+       this.dialogRef.close();
       },
     error: (error: any) => {
       console.error('Error claiming ticket: ', error);
       }
     });
   }
+
+  closeDialog(){
+    this.dialogRef.close();
+  }
+
 }
