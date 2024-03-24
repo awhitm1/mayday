0f5a0c933feaf32b38907c0c65d5999f2316e0eb
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-23 19:18:07 -0500

    landing

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -9,6 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  heroImage1 = '../../../assets/1.jpeg';
+  heroImage2 = '../../../assets/2.jpeg';
+  scrollPosition = 0;
 
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event: any) {
+    this.scrollPosition = window.scrollY;
+  }
 
 }
