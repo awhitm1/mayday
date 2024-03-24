993fd8d117358ff57acc5df2499da507cee0a6b4
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-23 23:20:49 -0500

    .

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -11,15 +11,16 @@ import { Component, ElementRef, HostListener } from '@angular/core';
 export class LandingComponent {
   heroImages = ['../../assets/1.jpeg', '../../assets/2.jpg', '../../assets/3.jpg'];
   scrollPosition = 0;
-  heroHeights = [0, 0, 0];
+  windowHeight = window.innerHeight;
+  heroTops = [0, 0, 0];
 
   constructor(private el: ElementRef) {}
 
   @HostListener('window:scroll', ['$event'])
   scrollHandler() {
     this.scrollPosition = window.scrollY;
-    for (let i = 0; i < this.heroHeights.length; i++) {
-      this.heroHeights[i] = this.el.nativeElement.querySelector(`#hero${i + 1}`).offsetTop;
+    for (let i = 0; i < this.heroTops.length; i++) {
+      this.heroTops[i] = this.el.nativeElement.querySelector(`#hero${i + 1}`).getBoundingClientRect().top + window.scrollY;
     }
   }
 
