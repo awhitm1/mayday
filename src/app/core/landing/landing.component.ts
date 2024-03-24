9f8a93dc445884b6e93cab90c49242919b5c0bcb
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-23 23:04:37 -0500

    .

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -9,11 +9,11 @@ import { Component, ElementRef, HostListener } from '@angular/core';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-  heroImages = ['../../../assets/1.jpeg', '../../../assets/2.jpg', '../../../assets/3.jpg']
+  heroImages = ['../../assets/1.jpeg ', '../../assets/2.jpg', '../../assets/3.jpg'];
   scrollPosition = 0;
-  heroHeights = [0,0,0];
+  heroHeights = [0, 0, 0];
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   @HostListener('window:scroll', ['$event'])
   scrollHandler() {
