3813e070fa63a7dfa45c1c4a7ba462d930fb4e2e
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-24 21:34:16 -0500

    landing final (for now)

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -10,18 +10,18 @@ import { Component, ElementRef, HostListener } from '@angular/core';
 })
 export class LandingComponent {
   heroImages = ['../../assets/1.jpeg', '../../assets/2.jpg', '../../assets/3.jpg'];
-  scrollPosition = 0;
-  windowHeight = window.innerHeight;
-  heroTops = [0, 0, 0];
+  // scrollPosition = 0;
+  // windowHeight = window.innerHeight;
+  // heroTops = [0, 0, 0];
 
-  constructor(private el: ElementRef) {}
+  // constructor(private el: ElementRef) {}
 
-  @HostListener('window:scroll', ['$event'])
-  scrollHandler() {
-    this.scrollPosition = window.scrollY;
-    for (let i = 0; i < this.heroTops.length; i++) {
-      this.heroTops[i] = this.el.nativeElement.querySelector(`#hero${i + 1}`).getBoundingClientRect().top + window.scrollY;
-    }
-  }
+  // @HostListener('window:scroll', ['$event'])
+  // scrollHandler() {
+  //   this.scrollPosition = window.scrollY;
+  //   for (let i = 0; i < this.heroTops.length; i++) {
+  //     this.heroTops[i] = this.el.nativeElement.querySelector(`#hero${i + 1}`).getBoundingClientRect().top + window.scrollY;
+  //   }
+  // }
 
 }
