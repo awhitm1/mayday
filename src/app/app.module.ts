d6625c621626c8ee8e37ed8d2848a4bce4dede3c
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-04-14 00:12:47 -0500

    create loading spinner w/ service, component, and interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authTokenInterceptor } from './auth-token.interceptor';
 import { NavComponent } from "./core/nav/nav.component";
 import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { ProfileComponent } from './features/profile/profile.component';
+import { SpinnerComponent } from "./shared/spinner/spinner.component";
+import { LoadingInterceptor } from './loading.interceptor';
 
 @NgModule({
     declarations: [
         AppComponent,
-
     ],
     providers: [
         provideHttpClient(withInterceptors([authTokenInterceptor])),
         provideAnimationsAsync(),
         {
-          provide: DATE_PIPE_DEFAULT_OPTIONS,
-          useValue: { dateFormat: 'medium'}
+            provide: DATE_PIPE_DEFAULT_OPTIONS,
+            useValue: { dateFormat: 'medium' }
+        },
+        {
+          provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
         }
     ],
     bootstrap: [AppComponent],
@@ -28,7 +32,8 @@ import { ProfileComponent } from './features/profile/profile.component';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        NavComponent, ProfileComponent
+        NavComponent, ProfileComponent,
+        SpinnerComponent
     ]
 })
 export class AppModule { }
---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
+
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class SpinnerComponent {
+
+  constructor(public loaderService: LoaderService) { }
+}
